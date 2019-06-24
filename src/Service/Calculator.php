2123aa97fa3ddca67e281212bb3dffd4ce9e2a3e
kunsangabo ndongala <berfy@simpleandnew.com>
2019-06-24 15:00:20 +0200

    trying to build test

---
+++ src/Service/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace App\Service;
+class Calculator
+{
+  public function add(int $firstNumber, int $secondNumber): int
+  {
+    return $firstNumber+$secondNumber;
+  }
+}
+?>
